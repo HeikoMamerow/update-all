c984c87d4aadf30f4d007f5c8a91e554617cc571
Heiko Mamerow <mail@heikomamerow.de>
2024-11-16 19:54:28 +0100

    Update update-all.sh
    Add snap refresh and brwe update

--- update-all.sh
+++ update-all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove
+sudo apt-get update && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo snap refresh && brew upgrade
